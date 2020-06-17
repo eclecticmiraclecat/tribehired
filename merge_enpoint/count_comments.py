9a59b20a908409510950b2647a731e8c1b8a8bf5
peanutduck <peanutduck@yahoo.com>
2020-06-18 00:19:49 +0800

    clean up

--- merge_enpoint/count_comments.py
+++ merge_enpoint/count_comments.py
@@ -10,8 +10,9 @@ posts_url = urllib.request.urlopen('https://jsonplaceholder.typicode.com/posts')
 comments = json.loads(comments_url.read())
 posts = json.loads(posts_url.read())
 
-counter_comments = Counter(row['postId'] for row in comments)
+counter_comments = Counter(comment['postId'] for comment in comments)
 
-with_comments = [ {'id':post['id'], 'title':post['title'], 'body':post['body'], 'total_comments': counter_comments[post['id']]} for post in posts]
+with_comments = [{'id': post['id'], 'title': post['title'], 'body': post['body'], 'total_comments': counter_comments[post['id']]} for post in posts]
 
 print(with_comments)
+
